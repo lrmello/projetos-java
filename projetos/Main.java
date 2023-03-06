5944d2848103a73ca839d82a3c33f29ab00ad4a8
lrmello <leonardo.reis28051995@gmail.com>
2023-03-06 19:43:30 -0300

    Arquivos iniciais

--- projetos/Main.java
+++ projetos/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+package projetos;
+public class Main {
+    public static void main(String[] args) {
+        minhaClasse minhaInstancia = new minhaClasse(10,"exemplo");
+        System.out.println(minhaInstancia.getAtributo1());
+        System.out.println(minhaInstancia.getAtributo2());
+        minhaInstancia.setAtributo1(20);
+        minhaInstancia.setAtributo2("Leonardo");
+        System.out.println(minhaInstancia.getAtributo1());
+        System.out.println(minhaInstancia.getAtributo2());
+        minhaInstancia.metodo1();
+        int resultado = minhaInstancia.metodo2(6);
+        System.out.println(resultado);
+    }
 }
--- projetos/minhaClasse.java
+++ projetos/minhaClasse.java
@@ -1,2 +1,39 @@
-package projetos;public class minhaClasse {
+package projetos;
+
+public class minhaClasse {
+    private int atributo1;
+    private String atributo2;
+
+    public minhaClasse(int i, String exemplo) {
+    }
+
+    public void MinhaClasse(int atributo1, String atributo2){
+        this.atributo1 = atributo1;
+        this.atributo2 = atributo2;
+    }
+
+    public int getAtributo1(){
+        return atributo1;
+    }
+
+    public void setAtributo1(int atributo1){
+        this.atributo1 = atributo1;
+    }
+
+    public String getAtributo2(){
+        return atributo2;
+    }
+
+    public void setAtributo2(String atributo2){
+        this.atributo2 = atributo2;
+    }
+
+    public void metodo1(){
+        
+    }
+    
+    public int metodo2(int parametro){
+        return parametro * 2;
+    }
+
 }
