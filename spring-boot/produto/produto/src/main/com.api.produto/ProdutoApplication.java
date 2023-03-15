dd1a3d9b7f21e02b9eee66bf9c6f0f91f1b10e54
lrmello <leonardo.reis28051995@gmail.com>
2023-03-15 17:05:52 -0300

    Incluindo Maven com persistence

---
+++ spring-boot/produto/produto/src/main/com.api.produto/ProdutoApplication.java
@@ -0,0 +1,45 @@
+package com.api.produto.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+@Entity
+@Table(name="produtos")
+public class ProdutoModelo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="codigo")
+    private int codigo;
+    @Column(name="nome")
+    private String nome;
+    @Column(name="valor")
+    private double valor;
+
+    /*Getters e Setters*/
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
